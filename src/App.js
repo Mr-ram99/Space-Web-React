4e7366eb7869f77cc1cd2c587b645ba5b7f832f4
Mr-ram9 <jrchoudhary2410@gmail.com>
2021-10-06 22:22:42 +0530

    Removed galllary

--- src/App.js
+++ src/App.js
@@ -4,19 +4,10 @@ import APOD from './Components/APOD';
 import Gallary from './Components/Gallary';
 const  App = () =>{
   const api_key="fWl7QxeLYzhQaU5NPZhDSysxDU63MUSVcRD1zZhp";
-  const curiosity_launch_date = new Date("06-08-2012");
-    const today_date = new Date();
-    const calculateSol =(date1,date2)=>{
-        let millis = date1 - date2;
-        let sol = Math.floor(millis/88775244.089999)-58;
-        return sol;
-    }
-    const sol = calculateSol(today_date,curiosity_launch_date);
   return (
    <> 
      <Navbar />
-     {/* <APOD api_key={api_key}/> */}
-     <Gallary api_key={api_key} sol={sol}/>
+     <APOD api_key={api_key}/>
    </>
   );
 }
--- src/Components/APOD.js
+++ src/Components/APOD.js
@@ -1,19 +1,19 @@
 import React, { useState, useEffect } from 'react'
 
 const APOD = (props) => {
-    const [parsedData, setParsedData] = useState([])
+    const [parsedData, setParsedData] = useState([]);
     const updateAPOD = async () => {
         const url = `https://api.nasa.gov/planetary/apod?api_key=${props.api_key}`;
-        const data = await fetch(url);
-        const parsedData = await data.json();
-        setParsedData(parsedData);
+        let data = await fetch(url);
+        let parseData = await data.json();
+        setParsedData(parseData);
     }
     useEffect(() => {
         updateAPOD();
-    })
+    },[]);
     return (
         <>
-            <div className="container-fluid apod-container text-light row m-0 py-4">
+           <div className="container-fluid apod-container text-light row m-0 py-4">
                 <div className="apod-desc col-sm-6 my-auto">
                     <p>
                         {parsedData.explanation}
@@ -21,7 +21,7 @@ const APOD = (props) => {
                 </div>
                 <div className="col-sm-6 my-auto">
                     {parsedData.media_type === "video" && <iframe src={parsedData.url} width="560" height="315" className="iframe" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>}
-                    {parsedData.media_type === "image" && <img src={parsedData.url} alt="Astronomy pic of the day" />}
+                    {parsedData.media_type === "image" && <img width="100%" src={parsedData.url} alt="Astronomy pic of the day" />}
                     <figcaption className="text-center">&copy; {parsedData.copyright}</figcaption>
                 </div>             
             </div>
